6d4f3d637c0d9102d053e9c57e6f147b9f739c13
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-20 19:41:45 -0500

    built out models

---
+++ todoList/todoList-model.js
@@ -0,0 +1,46 @@
+const db = require('../database/dbConfig')
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db('todos');
+}
+
+function findById(id) {
+  return db('todos')
+    .where({ id })
+    .first();
+}
+
+function findSteps(todoId) {
+  return db('steps as st')
+    .join('todos as t', 't.id', 'st.todo_id')
+    .where({ todo_id: todoId });
+}
+
+function add(todo) {
+  return db('todos')
+    .insert(todo, 'id')
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+function update(changes, id) {
+    return db('todos')
+      .where({ id })
+      .update(changes);
+  }
+  
+  function remove(id) {
+    return db('todos')
+      .where('id', id)
+      .del();
+  }
+
