f412646bca357860f86c47c1298b988ab1e926d8
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-23 15:18:15 -0500

    added console logs

--- auth/check-admin.js
+++ auth/check-admin.js
@@ -1,6 +1,8 @@
 module.exports = admin => {
     return (req, res, next) => {
       // check that the role that was in the token is the role passed as an argument?
+      console.log(req.admin);
+      console.log(admin)
       if (admin === req.admin.admin) {
         next();
       } else {
