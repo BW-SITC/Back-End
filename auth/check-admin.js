0b15106907344c5b4d517688e1c4fea143170c69
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-23 15:39:36 -0500

    took out console.log

--- auth/check-admin.js
+++ auth/check-admin.js
@@ -1,8 +1,6 @@
 module.exports = () => {
     return (req, res, next) => {
       // check that the role that was in the token is the role passed as an argument?
-      console.log(req.admin);
-      console.log(admin)
       if ( req.user.username === "admin") {
         next();
       } else {
