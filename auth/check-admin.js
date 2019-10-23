e0d9ba72944841958a2500002ff639382f284752
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-23 15:26:48 -0500

    getting admin middleware to work

--- auth/check-admin.js
+++ auth/check-admin.js
@@ -1,9 +1,9 @@
-module.exports = admin => {
+module.exports = () => {
     return (req, res, next) => {
       // check that the role that was in the token is the role passed as an argument?
       console.log(req.admin);
       console.log(admin)
-      if (admin === req.admin.admin) {
+      if ( req.user.username === "admin") {
         next();
       } else {
         res.status(403).json({ you: "can't touch this" });
